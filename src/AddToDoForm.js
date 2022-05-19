d14220b69a308bcdbf7e365005426b2b0bb45c99
Hadeer Zohdi <hadeer.zohdi@gmail.com>
2022-05-19 21:29:44 +0200

    updating form

--- src/AddToDoForm.js
+++ src/AddToDoForm.js
@@ -1,52 +1,69 @@
 import { useState } from "react";
 
 const AddToDoForm = ({ onAddTodo }) => {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [important, setImportant] = useState(false);
 
-   const [title, setTitle] = useState('');
-   const [date, setDate] = useState('');
-   const [important, setImportant] = useState(false);
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
 
-   const titleChangeHandler = (event) => {
-       setTitle(event.target.value)
-   }
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+  };
 
-   const dateChangeHandler = (event) => {
-        setDate(event.target.value)
-   }
+  const importantChangeHandler = () => {
+    setImportant(!important);
+  };
 
-   const importantChangeHandler = () => {
-        setImportant(!important)
-   }
-
-   const onSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
-    onAddTodo({title, date, important});
-    setTitle('')
-    setDate('')
-    setImportant(false)
-   }
+    if (!title) {
+      alert("Please enter a ToDo");
+      return;
+    }
+    onAddTodo({ title, date, important });
+    setTitle("");
+    setDate("");
+    setImportant(false);
+  };
 
-    return (
-        <form className="add-form" onSubmit={onSubmit}>
-            <div className="form-control">
-                <label>ToDo Title</label>
-                <input type="text" placeholder="Add a ToDo Title" value={title} onChange={titleChangeHandler}/>
-            </div>
-            <div className="form-control">
-                <label>ToDo Date</label>
-                <input type="text" placeholder="Add a ToDo Date" value={date} onChange={dateChangeHandler} />
-            </div>
-            <div className="important-ceckbox">
-                    <label>
-                        Important?
-                        <input type="checkbox" checked={important} onChange={importantChangeHandler}/>
-                    </label>
-                <p>Double Click the ToDo to set to Important</p>
-            </div>
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+      <div className="form-control">
+        <label>ToDo Title</label>
+        <input
+          type="text"
+          placeholder="Add a ToDo Title"
+          value={title}
+          onChange={titleChangeHandler}
+        />
+      </div>
+      <div className="form-control">
+        <label>ToDo Date</label>
+        <input
+          type="text"
+          placeholder="Add a ToDo Date"
+          value={date}
+          onChange={dateChangeHandler}
+        />
+      </div>
+      <div className="important-ceckbox">
+        <label>
+          Important?
+          <input
+            type="checkbox"
+            checked={important}
+            onChange={importantChangeHandler}
+          />
+        </label>
+        <p>Double Click the ToDo to set to Important</p>
+      </div>
 
-            <input type="submit" value="Save ToDo" className="btn btn-black" />
-        </form>
-    )
-}
+      <input type="submit" value="Save ToDo" className="btn btn-black" />
+    </form>
+  );
+};
 
 export default AddToDoForm;
